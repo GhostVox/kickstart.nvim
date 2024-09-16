447a0e5a36ab039dfa5e8f049b426abaa2ae6081
Brent-the-carpenter <beh337@gmail.com>
2024-09-15 20:25:58 -0400

    add custom key binds

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,14 +1,14 @@
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
+---- See the kickstart.nvim README$ for more information
 
 return {
   {
-    'kdheepak/lazygit.nvim',
-    lazy = false, -- Load the plugin immediately,
+    'tpope/vim-fugitive',
+    lazy = false, -- Load the plugin immediately
     config = function()
-      vim.keymap.set('n', '<leader>lg', ':LazyGit<CR>', { noremap = true, silent = true })
+      -- Example keybinding for :Gstatus
+      vim.keymap.set('n', '<leader>gs', ':Gstatus<CR>', { noremap = true, silent = true })
     end,
   },
 }
