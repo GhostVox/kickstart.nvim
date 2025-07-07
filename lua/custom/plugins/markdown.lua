703b81f07617fabd3561693dc37cc0cd14afa8ab
Brent-the-carpenter <beh337@gmail.com>
2025-07-07 18:49:37 -0400

    updated nvim setup

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,14 @@
+return {
+    'iamcco/markdown-preview.nvim',
+    ft = { 'markdown' },
+    build = 'cd app && npm install',
+    config = function()
+        -- Enable dark mode base
+        vim.g.mkdp_theme = 'dark'
+
+        -- Use custom CSS with Catppuccin colors
+        vim.g.mkdp_preview_options = {
+            custom_css = vim.fn.expand('~/.config/nvim/markdown-catppuccin.css'),
+        }
+    end,
+}
