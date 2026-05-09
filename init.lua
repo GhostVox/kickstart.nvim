3b86ad2ec0a6a2f450b97adbcf3fd57399c5d0c9
Brent Harrington <Ghostvox@icloud.com>
2026-05-08 21:17:18 -0400

    Added entire buffer yanking in init.lua

--- init.lua
+++ init.lua
@@ -272,6 +272,7 @@ else
     end,
   })
 
+  vim.keymap.set('n', '<leader>yae', function() vim.cmd ':%y' end)
   -- [[ Install `lazy.nvim` plugin manager ]]
   --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
   local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
